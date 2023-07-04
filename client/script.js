1f6a69b3854e2b92da4baa51c5f490d955757f44
Conor Dawson <conordawson92@googlemail.com>
2023-07-04 11:32:28 +0200

    update

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handleSubit = async (e) => {
 
   loader(messageDiv);
 
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://codex-7gig.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
